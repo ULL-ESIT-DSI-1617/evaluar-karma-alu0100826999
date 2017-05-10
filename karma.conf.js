cfe2d3589e36ca4be7a182311f0aae2fe8333194
alu0100826999 <alu0100826999@ull.edu.es>
2017-05-10 10:06:20 +0000

    g

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/tests.html',
+      'test/index.html',
       'test/*.js',
       'conversor/Temperature.js'
     ],
